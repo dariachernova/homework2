649bf12b93b730020f82ca44b19b341ae93438db
dariachernova <dariaiakovleva10u1@gmail.com>
2022-11-12 01:11:39 +0300

    done

---
+++ homework2.js
@@ -0,0 +1,19 @@
+let a = 2;
+let b = 3;
+let c = 4;
+
+if (a>b) {
+    if (a>c) {
+        console.log(a);
+    } else {
+        console.log(c);
+    }
+        
+} else {
+    if (b>c) {
+        console.log(b);
+    } else {
+        console.log(c);
+    }
+}
+
